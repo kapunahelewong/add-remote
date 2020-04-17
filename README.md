d6e73436170b3421d6300e19861f44ee2eb1ed65
Kapunahele Wong <kapunahelewong@gmail.com>
2020-04-17 15:01:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Add Remote
+
+This script helps you create a remote to a contributor's fork of the Angular repo, fetches the person's work, and checks out a new branch for you.
+
+## How to use this script:
+
+1. Download this repo.
+  **Important:** Store this directory at the same level as the Angular directory, **not within your Angular clone**.
+1. Go to terminal into the `add-remote` directory.
+1. Type `chmod +x add-remote.sh`
+1. Go back into your Angular directory.
+1. To run the script, type `../add-remote/add-remote.sh`
+
+### Add Remote prompts you for three things:
+
+1. The user's GitHub handle.
+  For example, mine is `kapunahelewong`
+1. The name of the person's branch as it appears on their fork.
+  For example, in this URL: `https://github.com/kapunahelewong/angular/tree/patch-1`, my branch name is `patch-1`.
+  You can see this in their PR under the PR title; for example, `kapunahelewong:patch-1`.
+1. What you plan on calling this branch on your computer.
+  Since `patch-1` is a common branch name, you might like to refer to it in a way that differentiates it from other `patch-1` branches.
+  While you could call it `patch-1`, You could also call it something different such as `kw-patch-1` or `kapunahele-patch-1`.
+
+When you're done editing, amending a commit, or rebasing use `git push` to update the PR.
+If you have used a unique name for the local version of the branch, get will give you an option of two commands.
+Use the first command which is as follows:
+
+```sh
+git push <user handle> HEAD:<real branch name on github>
+```
+
---
+++ add-remote.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# Bash script for adding someone else's fork as a remote to your Angular clone and checking out a specified branch.
+#
+#
+
+
+bold=`tput bold`
+normal=`tput sgr0`
+clear
+echo
+echo
+echo -e "\033[30;42m************************************************"
+echo -e "                                                "
+echo -e "                  ${bold} Add Remote${normal}\033[30;42m                   "
+echo -e "                                                "
+echo -e "          Add remote and checkout               "
+echo -e "             a specific branch.                 "
+echo -e "                                                "
+echo -e "              Kapunahele Wong                   "
+echo -e "                                                "
+echo -e "                                                "
+echo -e "************************************************\033[0m"
+echo
+
+echo "${bold}Run this script inside your local Angular clone."
+echo "Enter the user's handle: ${normal}"
+read user_name
+echo
+echo "${bold}Enter the name of the person's branch"
+echo "as it appears on their fork: ${normal}"
+read branch
+echo
+echo
+echo "${bold}Enter the name of the person's branch"
+echo "as you will refer to it locally.${normal}"
+read your_branch
+echo
+echo
+
+# Check if remote exists, if so, skip adding remote
+# List remotes and check if desired remote already exists
+
+# Make remotes array
+IFS=$'\n' read -r -d '' -a remotes < <( git remote && printf '\0' )
+
+# print all remotes on separate lines
+printf -- "%s\n" "${remotes[@]}"
+
+# If the remote exists skip adding the remote. If it doesn't exist and the remote.
+if [[ " ${remotes[@]} " =~ " ${user_name} " ]]; then
+  echo "Remote already exists. Fetching branch…"
+else
+  echo "Adding new remote..."
+  # Create an alias for the contributor's remote.
+  git remote add $user_name https://github.com/$user_name/angular.git
+  echo "$user_name added."
+fi
+
+# Fetch the PR branch.
+git fetch $user_name $branch
+
+# Create a new local branch based on their branch.
+git checkout -b $your_branch $user_name/$branch
